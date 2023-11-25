8886069cbcf6eb4d390b193a7d3e5eb3bc8e00f3
VictorVV123 <viktr.vlasjuk1@yandex.ru>
2023-11-25 21:55:49 +0300

    Initialize project using Create React App

---
+++ src/component/textInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './inputStyle.module.css'
+
+export const TextInput = () => {
+    return (
+        <div className={'test'}>
+            <div className={'test'}><input/></div>
+        </div>
+    );
+};
+
+
